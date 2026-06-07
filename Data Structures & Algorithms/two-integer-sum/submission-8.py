34c0f1f971f54d9e4e0b616e7ba88e8802d8a1c4
Ben Khant <158017055+benkhant@users.noreply.github.com>
2026-06-07 16:28:02 -0400

    Add: two-integer-sum - submission-8

---
+++ Data Structures & Algorithms/two-integer-sum/submission-8.py
@@ -0,0 +1,19 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        count = {}
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in count:
+                return [count[complement], i]
+            count[nums[i]] = i
+
+        # Time: O(n)
+        # Space: O(n)
+
+        # for i in range(len(nums)):
+        #     for j in range(i + 1, len(nums)):
+        #         if nums[i] + nums[j] == target:
+        #             return [i, j]
+
+        # Time: O(n^2)
+        # Space: O(1)
