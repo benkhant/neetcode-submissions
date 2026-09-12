8d66e1e084ef98c6c10a7cf53b9c68926aedb985
Ben Khant <158017055+benkhant@users.noreply.github.com>
2026-09-12 19:40:03 -0400

    Add: 4sum - submission-2

---
+++ Data Structures & Algorithms/4sum/submission-2.py
@@ -0,0 +1,44 @@
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        nums.sort()
+        res = []
+
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            for j in range(i+1, len(nums)):
+                if j > i + 1 and nums[j] == nums[j-1]:
+                    continue
+                l, r = j + 1, len(nums) - 1
+                while l < r: 
+                    total = nums[i] + nums[j] + nums[l] + nums[r]
+                    if total == target:
+                        res.append([nums[i], nums[j], nums[l], nums[r]])
+                        l += 1
+                        r -= 1
+                        while l < r and nums[l] == nums[l-1]:
+                            l += 1
+                    elif total < target:
+                        l += 1
+                    else:
+                        r -= 1
+        return res
+
+        # Time: O(n^3)
+        # Space: O(1)
+
+        # nums.sort()
+        # res = set()
+
+        # for i in range(len(nums)):
+        #     for j in range(i+1, len(nums)):
+        #         seen = set()
+        #         for k in range(j+1, len(nums)):
+        #             compliment = target - nums[i] - nums[j] - nums[k]
+        #             if compliment in seen:
+        #                 res.add(tuple(sorted([nums[i], nums[j], nums[k], compliment])))
+        #             seen.add(nums[k])
+        # return [list(t) for t in res]
+
+        # Time: O(n^3)
+        # Space: O(n)
